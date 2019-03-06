17f063e95e239022751ef9abf81e13c4458ba9f7
Oleksandr Nenko <alex.nenko@gmail.com>
2019-03-06 19:03:38 +0200

    Draft page added

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Test project to view JSON coded server side data in JQuery DataTables
 Draft is pure poor HTML with jQuery.
 Next step probably Angular SPA.
 
+TODO:
+1) time in table cell in minutes, lets design pop up on mouse over to show it as Xd YYh ZZm
+
---
+++ lstock.json.js
@@ -0,0 +1,28 @@
+
+var aDemoItems =
+
+[{
+  name: "AAA",
+  output1: "Lea",	// "DF", "MP", "GS", "OP", 
+  output2: "TM",	// "DF", "MP", "GS", "OP", 
+  out2_10: 10,
+  seedling: "ABCD",
+  price_s: 1.45,
+  price_vb: 90,
+  bundle: "N/A",
+  bndl_prof: 0,
+  t_sngl_min: 10,
+  t_bndl_min: 45,
+  t_farm_min: 33
+},{
+  name: "Qui",
+  output: "GG",	// "DF", "MP", "GS", "OP", 
+  out_10: 40,
+  seed: "Qui seed",
+  price_s: 1.45,
+  price_vb: 90,
+  bndl_prof: 50000,
+  t_sngl_min: 10,
+  t_bndl_min: 45,
+  t_farm_min: 540
+} ];
